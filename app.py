b83273eea5f4388fc5609df7f88ef49249116025
Josh Glen <129418264+JG21243@users.noreply.github.com>
2024-04-12 00:02:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -447,8 +447,8 @@ async def get_trace(body: GetTraceBody):
         }
     return await aget_trace_url(str(run_id))
 
-
+import uvicorn
 if __name__ == "__main__":
-    import uvicorn
+   
 
     uvicorn.run(app, host="0.0.0.0", port=8080)
