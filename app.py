05cb58bf5464a06ddd9d3b904454d12295f0f071
Josh Glen <129418264+JG21243@users.noreply.github.com>
2024-04-15 23:33:56 -0400

    Update app.py

--- app.py
+++ app.py
@@ -54,14 +54,14 @@ Generate a comprehensive and informative, yet concise answer of 1000 words or le
 given question based solely on the provided search results (URL and content). You must \
 only use information from the provided search results. Use an unbiased and \
 objective tone. Combine search results together into a coherent answer. Do not \
-repeat text. Cite search results using [${{number}}] notation. Only cite the most \
+repeat text. Cite search results using [{{number}}] notation. Only cite the most \
 relevant results that answer the question accurately. Place these citations at the end \
 of the sentence or paragraph that reference them - do not put them all at the end. If \
 different results refer to different entities within the same name, write separate \
 answers for each entity. If you want to cite multiple results for the same sentence, \
-format it as `[${{number1}}] [${{number2}}]`. However, you should NEVER do this with the \
+format it as `[{{number1}}] [{{number2}}]`. However, you should NEVER do this with the \
 same number - if you want to cite `number1` multiple times for a sentence, only do \
-`[${{number1}}]` not `[${{number1}}] [${{number1}}]`
+`[{{number1}}]` not `[{{number1}}] [{{number1}}]`
 
 You should use bullet points in your answer for readability. Put citations where they apply \
 rather than putting them all at the end.
